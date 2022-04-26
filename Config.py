d6a47b7cc8659c09f227dd93007808f0019e4177
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-04-26 18:44:03 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -9,7 +9,7 @@ if ENVIRONMENT:
         raise Exception("Your API_ID is not a valid integer.")
     API_HASH = os.environ.get('API_HASH', '4e984ea35f854762dcde906dce426c2d')
     BOT_TOKEN = os.environ.get('BOT_TOKEN', '5335343977:AAFzt2TBZ7fiVo1sG1pBNhbjjjF6GHECVPE')
-    DATABASE_URL = os.environ.get('DATABASE_URL', None)
+    DATABASE_URL = os.environ.get('DATABASE_URL', "postgres://xcgmuzrq:uMhAxXtJgmVPqLav7Y37LFQ9wAqKd8CT@isilo.db.elephantsql.com/xcgmuzrq")
     DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")  # Sqlalchemy dropped support for "postgres" name.
     # https://stackoverflow.com/questions/62688256/sqlalchemy-exc-nosuchmoduleerror-cant-load-plugin-sqlalchemy-dialectspostgre
     MUST_JOIN = os.environ.get('MUST_JOIN', Superior_Bots)
@@ -20,7 +20,7 @@ else:
     API_ID = 6435225
     API_HASH = "4e984ea35f854762dcde906dce426c2d"
     BOT_TOKEN = "5335343977:AAFzt2TBZ7fiVo1sG1pBNhbjjjF6GHECVPE"
-    DATABASE_URL = ""
+    DATABASE_URL = "postgres://xcgmuzrq:uMhAxXtJgmVPqLav7Y37LFQ9wAqKd8CT@isilo.db.elephantsql.com/xcgmuzrq"
     DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")
     MUST_JOIN = "Superior_Bots"
     if MUST_JOIN.startswith("@"):
