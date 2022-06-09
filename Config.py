e1fee32c0ff1f2e2004032dcb59faa71dca96366
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-06-09 21:25:02 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -8,7 +8,7 @@ if ENVIRONMENT:
     except ValueError:
         raise Exception("Your API_ID is not a valid integer.")
     API_HASH = os.environ.get('API_HASH', '4e984ea35f854762dcde906dce426c2d')
-    BOT_TOKEN = os.environ.get('BOT_TOKEN', '5338702754:AAGVDCXMp_QO8YujSK5NSseCJ5r05j6zElM')
+    BOT_TOKEN = os.environ.get('BOT_TOKEN', '5335343977:AAFzt2TBZ7fiVo1sG1pBNhbjjjF6GHECVPE')
     DATABASE_URL = os.environ.get('DATABASE_URL', "postgres://xcgmuzrq:uMhAxXtJgmVPqLav7Y37LFQ9wAqKd8CT@isilo.db.elephantsql.com/xcgmuzrq")
     DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")  # Sqlalchemy dropped support for "postgres" name.
     # https://stackoverflow.com/questions/62688256/sqlalchemy-exc-nosuchmoduleerror-cant-load-plugin-sqlalchemy-dialectspostgre
@@ -19,7 +19,7 @@ else:
     # Fill the Values
     API_ID = 6435225
     API_HASH = "4e984ea35f854762dcde906dce426c2d"
-    BOT_TOKEN = "5338702754:AAGVDCXMp_QO8YujSK5NSseCJ5r05j6zElM"
+    BOT_TOKEN = "5335343977:AAFzt2TBZ7fiVo1sG1pBNhbjjjF6GHECVPE"
     DATABASE_URL = "postgres://xcgmuzrq:uMhAxXtJgmVPqLav7Y37LFQ9wAqKd8CT@isilo.db.elephantsql.com/xcgmuzrq"
     DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")
     MUST_JOIN = "TheUpdatesChannel"
